e64ddea50cb6314bb86940d83e958bec9b6ec101
WALLEY <rajiolawale2020@gmail.com>
2018-08-01 17:04:37 +0100

    added receipt

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var methodOverride = require("method-override");
 var fileParser = require('connect-multiparty')();
 var request = require("request");
 var bodyParser = require("body-parser");
-
-var apiKey = "FLWPUBK-02ad06ed9671c20dc78057a1d72ddb33-X";
-var seckey = "FLWSECK-06da147416f0a6788c422ed2acec5016-X";
 app.set("port", process.env.PORT || 8000);
 app.set("view engine","ejs");
 app.set("views","views");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+require('dotenv').config({ path: 'variables.env' });
+var apiKey = "FLWPUBK-02ad06ed9671c20dc78057a1d72ddb33-X";
+var seckey = "FLWSECK-06da147416f0a6788c422ed2acec5016-X";
 app.get("/",function (req,res) {
     request("https://ravesandboxapi.flutterwave.com/banks",{json:true}, function (error, response, body) {
         if (error) throw new Error(error);
@@ -31,14 +31,14 @@ app.post("/post", function (req, res) {
     var account_number = req.body.account_number;
     var narration = req.body.narration;
     var amount = req.body.amount;
-    var seckey = "FLWSECK-06da147416f0a6788c422ed2acec5016-X";
+     var seckey = "FLWSECK-06da147416f0a6788c422ed2acec5016-X";
     var currency = "NGN";
     var ref = "mk-902837-jk";
     var makeTransfer = {
         account_bank: account_bank,
         account_number: account_number,
         amount: amount,
-        seckey: "FLWSECK-06da147416f0a6788c422ed2acec5016-X",
+        seckey: seckey,
         narration: narration,
         currency: currency,
         ref: ref   
@@ -64,11 +64,29 @@ app.post("/post", function (req, res) {
         if (error) throw new Error(error);
 
         console.log(body);
-        res.redirect("/");
+        res.redirect("/receipt");
     });
 
     
 });
+app.get("/receipt",function (req,res) {
+    var options = {
+  qs: {seckey:'FLWSECK-06da147416f0a6788c422ed2acec5016-X'},
+  headers: { 'content-type': 'application/json' }
+};
+
+request('https://ravesandboxapi.flutterwave.com/v2/gpx/transfers',options, function (error, response, body) {
+  if (error) throw new Error(error);
+    var parseData=JSON.parse(body);
+  console.log(parseData["data"]["transfers"]);
+    var content = parseData["data"]["transfers"];
+  res.render("receipt",{
+      content: content
+  });
+
+});
+});
+
 var server =app.listen(app.get("port"),function () {
     console.log("you are listening to port " + app.get("port"));
 });
